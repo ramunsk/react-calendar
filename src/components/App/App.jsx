a05ad0f5d5836f653ac6370b7732a7a6f5e8e8a6
Ramunas <514899+ramunsk@users.noreply.github.com>
2019-11-27 08:50:09 +0200

    Fixed unit tests

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Footer from '../Footer/Fotter';
+import Footer from '../Footer/Footer';
 import Header from '../Header/Header';
 import MonthView from '../MonthView/MonthView';
 import './App.css';
