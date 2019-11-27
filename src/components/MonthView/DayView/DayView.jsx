38102cd275fc9c90c3e7f41a3154771b0e064a33
Ramunas <514899+ramunsk@users.noreply.github.com>
2019-11-27 21:30:32 +0200

    Added dayview component

---
+++ src/components/MonthView/DayView/DayView.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './DayView.css';
+
+const DayView = () => {
+    return (
+        <div className="day">
+            <div className="day__header">Day</div>
+        </div>
+    );
+};
+
+export default DayView;
