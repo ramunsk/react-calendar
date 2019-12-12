2e7da85e56bd43c270e3c64c8079b09e9fa28d8f
Ramunas <514899+ramunsk@users.noreply.github.com>
2019-12-12 12:48:26 +0200

    Month component cleanup

--- src/components/Month/Month.jsx
+++ src/components/Month/Month.jsx
@@ -1,18 +1,9 @@
-import { eachDayOfInterval, endOfMonth, endOfWeek, startOfMonth, startOfWeek } from 'date-fns';
 import React from 'react';
-import Day from './Day/Day';
 import Days from './Days/Days';
 import './Month.css';
 import Weekdays from './Weekdays/Weekdays';
 
 const MonthView = ({ date }) => {
-    const now = date || new Date();
-    const firstDate = startOfWeek(startOfMonth(now), { weekStartsOn: 1 });
-    const lastDate = endOfWeek(endOfMonth(now), { weekStartsOn: 1 });
-    const viewDates = eachDayOfInterval({ start: firstDate, end: lastDate });
-
-    const dayViews = viewDates.map((d, i) => <Day key={d.valueOf()} date={d} />);
-
     return (
         <div className="month">
             <Weekdays />
